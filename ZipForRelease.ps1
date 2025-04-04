a03a7248543c4faaa8ad2bc4e84c98a807af334c
Randy Fellmy <git@coises.com>
2025-04-03 22:56:57 -0700

    Changed ZipForRelease.ps1 to work with standard gitignore for Visual Studio; puts zips and hashes list in top-level Releases directory, which is already ignored.

--- ZipForRelease.ps1
+++ ZipForRelease.ps1
@@ -1,12 +1,10 @@
 $Project = "ControlledAutoIndent"
+New-Item -Path Releases -ItemType Directory -Force
 $x86Version = (Get-Command ".vs\bin\Win32\Release\$Project.dll").FileVersionInfo.ProductVersion
-Compress-Archive -DestinationPath "$Project-$x86Version-x86.zip" -Path ".vs\bin\Win32\Release\*"
+Compress-Archive -DestinationPath "Releases\$Project-$x86Version-x86.zip" -Path ".vs\bin\Win32\Release\*"
 $x64Version = (Get-Command ".vs\bin\x64\Release\$Project.dll").FileVersionInfo.ProductVersion
-Compress-Archive -DestinationPath "$Project-$x64Version-x64.zip" -Path ".vs\bin\x64\Release\*"
-$x86Hash = (Get-FileHash "$Project-$x86Version-x86.zip" -Algorithm SHA256).Hash
-$x64Hash = (Get-FileHash "$Project-$x64Version-x64.zip" -Algorithm SHA256).Hash
+Compress-Archive -DestinationPath "Releases\$Project-$x64Version-x64.zip" -Path ".vs\bin\x64\Release\*"
+$x86Hash = (Get-FileHash "Releases\$Project-$x86Version-x86.zip" -Algorithm SHA256).Hash
+$x64Hash = (Get-FileHash "Releases\$Project-$x64Version-x64.zip" -Algorithm SHA256).Hash
 $Hashes =  "SHA-256 Hash ($Project-$x64Version-x64.zip): " + $x64Hash + "`nSHA-256 Hash ($Project-$x86Version-x86.zip): " + $x86Hash
-Write-Output $Hashes
-Write-Output ""
-Set-Clipboard -Value $Hashes
-Read-Host -Prompt "Press Enter to exit"
+Out-File -FilePath "Releases\Hashes.txt" -Append -InputObject $Hashes
